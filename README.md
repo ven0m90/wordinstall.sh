1c82beb5c8aff7d58c247570f82338537568db8b
Azam <107202529+ven0m90@users.noreply.github.com>
2024-05-07 07:13:58 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Wordlists Installer for Bug Bounty & Pentesting
+# Installation
+```console
+curl -L https://raw.githubusercontent.com/ven0m90/wordinstall.sh/main/wordinstall.sh -o wordinstall.sh;chmod +x wordinstall.sh;bash wordinstall.sh 
---
+++ wordinstall.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#colours
+BOLD="\e[1m"
+UNDERLINE="\e[4m"
+RED="\e[31m"
+GREEN="\e[32m"
+BLUE="\e[34"
+CYAN="\e[36m"
+NC="\e[0m"
+VERSION="2.0"  
+
+clear ;
+
+echo "
+
+
+██╗    ██╗ ██████╗ ██████╗ ██████╗ ██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗        ███████╗██╗  ██╗
+██║    ██║██╔═══██╗██╔══██╗██╔══██╗██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║        ██╔════╝██║  ██║
+██║ █╗ ██║██║   ██║██████╔╝██║  ██║██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║        ███████╗███████║
+██║███╗██║██║   ██║██╔══██╗██║  ██║██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║        ╚════██║██╔══██║
+╚███╔███╔╝╚██████╔╝██║  ██║██████╔╝██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗██╗███████║██║  ██║
+ ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝╚══════╝╚═╝  ╚═╝
+
+
+"|lolcat
+
+
+#-Update & Upgrade full
+echo -e $BOLD"[+] Updating Packages...$NC ${result}" | lolcat
+sudo apt install -y lolcat wget git curl toilet figlet ;
+sudo apt -y update ;sudo rm /var/lib/apt/lists/lock ; sudo apt autoremove -y  ; sudo apt clean ; sudo apt autoclean ;sudo apt -y upgrade 
+
+echo -e $BOLD"[+]Installing Wordlist...$NC ${result}" | lolcat
+git clone https://github.com/ven0m90/wordinstall.sh.git  ~/wordlist
+
+mkdir -p   ~/wordlist  ~/wordlist/dns  ~/wordlist/payloads-list  ~/wordlist/user-pass  ~/wordlist/words-list
+
+chmod +rwx ~/wordlist
+
+downloading_wordlist(){
+#fuzzing-ffuf-dirsearch
+wget https://raw.githubusercontent.com/six2dez/OneListForAll/main/onelistforallshort.txt  -P ~/wordlist/
+wget https://raw.githubusercontent.com/Bo0oM/fuzz.txt/master/extensions.txt  -P ~/wordlist/
+wget https://raw.githubusercontent.com/Bo0oM/fuzz.txt/master/fuzz.txt -P ~/wordlist/
+wget https://raw.githubusercontent.com/maurosoria/dirsearch/master/db/dicc.txt -P ~/wordlist/
+wget #subdomains-bruteforce-ffuf
+wget https://raw.githubusercontent.com/netsecurity-as/subfuz/master/subdomain_megalist.txt -P ~/wordlist/
+
+#dns-bruteforce-shuffeldns
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/DNS/deepmagic.com-prefixes-top500.txt subdomain-top500.txt -P ~/wordlist/dns/
+wget https://raw.githubusercontent.com/n0kovo/n0kovo_subdomains/main/n0kovo_subdomains_tiny.txt  -P subdomain50k.txt -P ~/wordlist/dns/
+wget https://wordlists-cdn.assetnote.io/data/manual/2m-subdomains.txt -P ~/wordlist/dns/
+wget https://wordlists-cdn.assetnote.io/data/manual/best-dns-wordlist.txt -P ~/wordlist/dns/
+wget https://gist.githubusercontent.com/jhaddix/86a06c5dc309d08580a018c66354a056/raw/96f4e51d96b2203f19f6381c8c545b278eaa0837/all.txt -P ~/wordlist/dns/
+
+
+#resolver
+sudo rm ~/wordlist/resolvers.txt ; curl -L https://raw.githubusercontent.com/trickest/resolvers/main/resolvers.txt -o ~/wordlist/resolvers.txt
+
+
+}
+downloading_wordlist
+
+echo -e $BOLD"[+]Done...$NC ${result}" | lolcat
+echo -e $BOLD"[+]Wordlist installed in ~/wordlist$NC ${result}" | lolcat
