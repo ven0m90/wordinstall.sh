e78dd7d649f1b3b577bb7b3f6080e5a93ee3acd6
Azam <107202529+ven0m90@users.noreply.github.com>
2024-08-09 19:40:29 +0530

    Update wordinstall.sh

--- wordinstall.sh
+++ wordinstall.sh
@@ -33,36 +33,30 @@ echo 'root' | sudo -kS  apt install -y lolcat wget git curl toilet figlet ;
 echo -e $BOLD"[+]Installing Wordlist...$NC ${result}" | lolcat
 git clone https://github.com/ven0m90/wordinstall.sh.git  ~/wordlist
 
-mkdir -p   ~/wordlist  ~/wordlist/dns  ~/wordlist/payloads-list  ~/wordlist/user-pass  ~/wordlist/words-list   ~/wordlist/parameters 
+mkdir -p   ~/wordlist  ~/wordlist/dns  ~/wordlist/payloads-list  ~/wordlist/user-pass  ~/wordlist/words-list   ~/wordlist/parameters  ~/wordlist/fuzzing
 
 chmod +rwx ~/wordlist
 
 downloading_wordlist(){
 
-git clone https://github.com/random-robbie/bruteforce-lists  ~/wordlist/bruteforce-lists
-git clone https://github.com/orwagodfather/WordList   ~/wordlist/gofather-wordlist
+git clone https://github.com/random-robbie/bruteforce-lists  ~/wordlist/fuzzing/bruteforce-lists
+git clone https://github.com/orwagodfather/WordList   ~/wordlist/fuzzing/gofather-wordlist
 
 #fuzzing-directory  
-wget https://raw.githubusercontent.com/six2dez/OneListForAll/main/onelistforallshort.txt  -P ~/wordlist/
-wget https://raw.githubusercontent.com/Bo0oM/fuzz.txt/master/extensions.txt  -P ~/wordlist/
-wget https://raw.githubusercontent.com/Bo0oM/fuzz.txt/master/fuzz.txt -P ~/wordlist/
-wget https://raw.githubusercontent.com/maurosoria/dirsearch/master/db/dicc.txt -P ~/wordlist/
-wget https://wordlists-cdn.assetnote.io/data/automated/httparchive_directories_1m_2024_05_28.txt -P ~/wordlist/
-wget  https://gist.githubusercontent.com/nullenc0de/96fb9e934fc16415fbda2f83f08b28e7/raw/146f367110973250785ced348455dc5173842ee4/content_discovery_nullenc0de.txt  -P ~/wordlist/
-wget https://raw.githubusercontent.com/emadshanab/admin-paths/main/admin.txt -P ~/wordlist/
-wget https://raw.githubusercontent.com/emadshanab/Headers-Bypass-List/main/Headers.txt -P ~/wordlist/
+wget https://raw.githubusercontent.com/six2dez/OneListForAll/main/onelistforallshort.txt  -P ~/wordlist/fuzzing/
+wget https://raw.githubusercontent.com/Bo0oM/fuzz.txt/master/fuzz.txt -P  ~/wordlist/fuzzing/
+wget https://raw.githubusercontent.com/maurosoria/dirsearch/master/db/dicc.txt -P  ~/wordlist/fuzzing/
+wget https://wordlists-cdn.assetnote.io/data/automated/httparchive_directories_1m_2024_05_28.txt -P  ~/wordlist/fuzzing/
+wget  https://gist.githubusercontent.com/nullenc0de/96fb9e934fc16415fbda2f83f08b28e7/raw/146f367110973250785ced348455dc5173842ee4/content_discovery_nullenc0de.txt  -P  ~/wordlist/fuzzing/
 
 #js
-wget https://wordlists-cdn.assetnote.io/data/automated/httparchive_js_2024_05_28.txt -P ~/wordlist/
+wget https://wordlists-cdn.assetnote.io/data/automated/httparchive_js_2024_05_28.txt -P ~/wordlist/fuzzing/
 #parameter
 wget https://wordlists-cdn.assetnote.io/data/automated/httparchive_parameters_top_1m_2024_05_28.txt -P  ~/wordlist/parameters/
 wget https://raw.githubusercontent.com/s0md3v/Arjun/master/arjun/db/large.txt -P  ~/wordlist/parameters/
 #dns-bruteforce-shuffeldns
 curl -sL https://raw.githubusercontent.com/guelfoweb/knock/master/knock/wordlist/wordlist.txt -o ~/wordlist/dns/kockpy10k.txt
 wget https://wordlists-cdn.assetnote.io/data/automated/httparchive_subdomains_2024_05_28.txt -P  ~/wordlist/dns/
-wget https://raw.githubusercontent.com/netsecurity-as/subfuz/master/subdomain_megalist.txt -P  ~/wordlist/dns/
-wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/DNS/deepmagic.com-prefixes-top500.txt subdomain-top500.txt -P ~/wordlist/dns/
-wget https://raw.githubusercontent.com/n0kovo/n0kovo_subdomains/main/n0kovo_subdomains_tiny.txt  -P  ~/wordlist/dns/
 wget https://wordlists-cdn.assetnote.io/data/manual/best-dns-wordlist.txt -P ~/wordlist/dns/
 wget https://gist.githubusercontent.com/jhaddix/f64c97d0863a78454e44c2f7119c2a6a/raw/96f4e51d96b2203f19f6381c8c545b278eaa0837/all.txt -P ~/wordlist/dns/
 wget https://wordlists-cdn.assetnote.io/data/manual/2m-subdomains.txt -P ~/wordlist/dns/
